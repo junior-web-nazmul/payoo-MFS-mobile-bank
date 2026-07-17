58773fbf5507f1e240e88ddb3531e96deba35ecf
Nazmul0197 <sirmd97@gmail.com>
2026-07-17 20:50:43 +0600

    add money validation added

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+const validPin = 1234;
+
 document
   .getElementById("btn-add-money")
   .addEventListener("click", function (e) {
@@ -6,11 +8,28 @@ document
     const bank = document.getElementById("bank").value;
     const accountNumber = document.getElementById("account-number").value;
     const amount = parseInt(document.getElementById("add-amount").value);
-    const pin = document.getElementById("add-pin").value;
+    const pin = parseInt(document.getElementById("add-pin").value);
 
     const availableBalance = parseInt(
       document.getElementById("available-balance").innerText,
     );
+    if (bank === "Select bank") {
+      alert("please select bank");
+      return;
+    }
+
+    if (accountNumber.length !== 13) {
+      alert("Please provide a valid account number");
+      return;
+    }
+    if (!amount || isNaN(amount) || amount <= 0) {
+      alert("Please enter a valid amount to add");
+      return;
+    }
+    if (pin !== validPin) {
+      alert("Please Provide valid pin");
+      return;
+    }
     const totalNewAvaiableBalance = availableBalance + amount;
     document.getElementById("available-balance").innerText =
       totalNewAvaiableBalance;
